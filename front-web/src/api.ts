38d366149f2058210c60e8122f05044ab334e017
Coimbra <adsjkbd@gmail.com>
2021-01-10 07:53:35 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -13,5 +13,5 @@ export function fetchLocalMapBox(local: string) {
 }
 
 export function saveOrder(payload: OrderPayload) {
-    return axios.post(`${API_URL}/orders123`, payload)
+    return axios.post(`${API_URL}/orders`, payload)
 }
